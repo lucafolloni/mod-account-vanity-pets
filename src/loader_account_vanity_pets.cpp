82813e291b641fd51e8cb0b0265440b683eb5c18
Luca Folloni <luca.folloni@gmail.com>
2025-03-09 11:16:59 +0100

    Create loader_account_vanity_pets.cpp

---
+++ src/loader_account_vanity_pets.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
+ *
+*/
+
+void AddAccountVanityPetsScripts();
+
+void Addmod_account_vanity_petsScripts()
+{
+    AddAccountVanityPetsScripts();
+}
